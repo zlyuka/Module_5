f631c0c55a40a94c0cde4b913ce71eada0d9ada9
zlyuka <zhenya0699@gmail.com>
2024-10-03 21:24:08 +0300

    Выполнено.

--- Module_5_1.py
+++ Module_5_1.py
@@ -1,10 +1,10 @@
 class House:
-    def __init__(self, name, numder_of_floor):
+    def __init__(self, name, numder_of_floors):
         self.name = name
-        self.numder_of_floor = numder_of_floor
+        self.numder_of_floors = numder_of_floors
 
     def go_to(self, new_floor):
-        if 1 > new_floor or new_floor > self.numder_of_floor:
+        if 1 > new_floor or new_floor > self.numder_of_floors:
             return print('Такого этажа не существует')
         elif new_floor:
             for i in range(1,new_floor+1):
--- Module_5_2.py
+++ Module_5_2.py
@@ -1,19 +1,19 @@
 class House:
-    def __init__(self, name, numder_of_floor):
+    def __init__(self, name, numder_of_floors):
         self.name = name
-        self.numder_of_floor = numder_of_floor
+        self.numder_of_floors = numder_of_floors
 
     def go_to(self, new_floor):
-        if 1 > new_floor or new_floor > self.numder_of_floor:
+        if 1 > new_floor or new_floor > self.numder_of_floors:
             return print('Такого этажа не существует')
         elif new_floor:
             for i in range(1,new_floor+1):
                 print(i)
     def __len__(self):
-        return self.numder_of_floor
+        return self.numder_of_floors
 
     def __str__(self):
-        return f'Название:{self.name}, количество этажей:{self.numder_of_floor}'
+        return f'Название:{self.name}, количество этажей:{self.numder_of_floors}'
 
 
 h1 = House('ЖК Эльбрус', 10)
