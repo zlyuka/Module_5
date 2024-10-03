e0ccf9aadfa380286c0681d068153a3d690b5c7a
zlyuka <zhenya0699@gmail.com>
2024-10-03 18:54:50 +0300

    Выполнено.

--- Module_5_1.py
+++ Module_5_1.py
@@ -1,18 +1,21 @@
+from math import floor
+
+
 class House:
     def __init__(self, name, numder_of_floor):
         self.name = name
         self.numder_of_floor = numder_of_floor
 
     def go_to(self, new_floor):
-        if 1 > new_floor and new_floor > self.numder_of_floor:
-            print('Такого этажа не существует')
+        if 1 > new_floor or new_floor > self.numder_of_floor:
+            return print('Такого этажа не существует')
         elif new_floor:
             for i in range(1,new_floor+1):
                 print(i)
 
 
 
-# h1 = House('ЖК Горский', 18)
+h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-#h1.go_to(5)
-h2.go_to(0)
+h1.go_to(5)
+h2.go_to(10)
